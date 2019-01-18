0772ba99621dd48b26287fd5ce796a3e4f6cf1b7
Issac Gable <issacgable@gmail.com>
2019-01-18 15:03:48 -0700

    fixed index.mjs to properly output functions

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -2,12 +2,6 @@ import $ from 'jquery'
 import css from '../css/template.scss'
 import milligram from 'milligram'
 
-export default function () {
-  let one, two, three
-  one = 1
-  two = 2
-  three = 3
-  if (one + two === three) {
-    console.log('True')
-  }
-}
+$(() => {
+  
+})
